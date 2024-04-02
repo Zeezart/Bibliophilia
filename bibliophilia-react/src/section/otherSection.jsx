2162978fd7b66ce5af756af291b3aea62810a3c3
Enibecky <beckytolu@gmail.com>
2024-04-02 16:10:22 +0100

    updated key-features

--- bibliophilia-react/src/section/otherSection.jsx
+++ bibliophilia-react/src/section/otherSection.jsx
@@ -1,9 +1,17 @@
 import React from "react"
+import Group from "../assets/Group.png"
 
 function OtherSection(){
     return(
         <>
-            <h1>MY CONTENTS HERE</h1>
+        <section className="key-features">
+            <div className="feature-div">
+            <p>Our Core Features</p>
+            <h1>Explore our Key Features</h1>
+            <img src={Group} alt="plus-group" className="plus"/>
+            </div>
+            
+        </section>
         </>
     )
 }
